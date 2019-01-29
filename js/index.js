8b9814422da443e604b60e37eaaeef88ff603785
Ian Be <ian84be@gmail.com>
2019-01-29 15:39:24 -0600

    add rainbow scroller

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 // STROBELIGHT BUTTONS
 let destinationButton = document.querySelectorAll('.destination .btn');
-console.log(destinationButton);
 destinationButton.forEach(el => {
     el.addEventListener('mousedown', ev => {
         document.querySelector('body').style = 'animation: pulse 500ms infinite';
@@ -9,3 +8,17 @@ destinationButton.forEach(el => {
         document.querySelector('body').style = 'animaion: null';
     })
 });
+
+window.addEventListener('scroll', ev => {
+    document.querySelector('h1.logo-heading').style.color = getColor();
+});
+
+// HEX CODE GENERATOR (student contribution from web16 video)
+function getColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i=0;i<6;i++) {
+        color += letters[Math.floor(Math.random()*16)];
+    }
+    return color;
+}
