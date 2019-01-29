5c223e7ec5887dc87dbf17b083f7da059572f754
Ian Be <ian84be@gmail.com>
2019-01-29 13:37:52 -0600

    domII initial PR

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+let destinationButton = document.querySelectorAll('.destination .btn');
+console.log(destinationButton);
+destinationButton.forEach(el => {
+    el.addEventListener('click', ev => {
+        console.log(`event type: ${ev} target is ${ev.target}`);
+    })
+});
